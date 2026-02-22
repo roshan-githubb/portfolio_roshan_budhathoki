6437fdc86935d608dff992c763f74761eed66465
Roshan Budhathoki <roshanbc9860@gmail.com>
2026-02-23 02:30:54 +0545

    Added chatbot config

--- components/About.tsx
+++ components/About.tsx
@@ -61,7 +61,7 @@ const About = () => {
               </p>
             </div>
           </div>
-
+            
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
             {highlights.map((item, index) => (
               <motion.div
